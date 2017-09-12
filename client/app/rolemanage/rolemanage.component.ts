069ced40707184841aa95f8c5c3482e4041e6a59
smartankit094@gmail.com <ankit.tiwari7>
2017-09-12 17:56:29 +0530

    update code

--- client/app/rolemanage/rolemanage.component.ts
+++ client/app/rolemanage/rolemanage.component.ts
@@ -2,19 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 import { ToastComponent } from '../shared/toast/toast.component';
 import { AuthService } from '../services/auth.service';
-import { UserService } from '../services/user.service';
+import { MangeroleService } from '../services/mangerole.service';
 import { routerTransition } from '../router.animations';
+
 
 @Component({
   selector: 'app-rolemanage',
   templateUrl: './rolemanage.component.html',
-  styleUrls: ['./rolemanage.component.scss']
+  styleUrls: ['./rolemanage.component.scss'],
+  animations: [routerTransition()]
 })
 export class RolemanageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( public toast: ToastComponent,private roleService: MangeroleService) { }
+  roles=[];
+  isLoading= true;
   ngOnInit() {
+    this.getRoles();
+  }
+
+
+
+
+  //get all  user details
+  getRoles() {
+    this.roleService.getRoles().subscribe(
+      data => this.roles = data,
+      error => console.log(error),
+      () => this.isLoading = false
+    );
   }
 
 }
--- client/app/rolemanage/rolemanage.module.ts
+++ client/app/rolemanage/rolemanage.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { PageHeaderModule } from '../shared';
 import { RolemanageRoutingModule } from './rolemanage-routing.module';
 import { RolemanageComponent } from './rolemanage.component';
+import { SharedModule } from '../shared/toast/shared.module';
+import { MangeroleService } from '../services/mangerole.service';
 @NgModule({
   imports: [
     CommonModule,
-    RolemanageRoutingModule
+    RolemanageRoutingModule,
+    PageHeaderModule,
+    SharedModule
   ],
+  providers:[MangeroleService],
   declarations: [RolemanageComponent]
 })
 export class RolemanageModule { }
