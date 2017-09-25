7e7720845492badc2521fd7d2742a5ebeaa573c4
smartankit094@gmail.com <ankit.tiwari7>
2017-09-25 09:44:30 +0530

    update code

---
+++ server/controllers/upload.js
@@ -0,0 +1,20 @@
+var multer = require('multer');
+var DIR = './uploads/';
+var upload = multer({ dest: DIR }).single('photo');
+var path = require('path');
+var UploadCtrl = (function () {
+    function UploadCtrl() {
+        this.upload = function (req, res, next) {
+            var path = '';
+            upload(req, res, function (err) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log(req.file);
+                res.sendStatus(200);
+            });
+        };
+    }
+    return UploadCtrl;
+})();
+exports["default"] = UploadCtrl;
